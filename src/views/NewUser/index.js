a82ea245a162d9726f2515353d334b67ae1d8031
Mayko Douglas <maykodouglasbr@outlook.com>
2021-02-18 19:19:08 -0300

    Criação de novas paginas e lib de icones

---
+++ src/views/NewUser/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './styles';
+
+import api from '../../services/api';
+
+function NewUser() {
+
+  return (
+    <S.Container>
+     
+    </S.Container>
+  )
+}
+
+export default NewUser;
---
+++ src/views/User/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as S from './styles';
+
+import api from '../../services/api';
+
+function User() {
+
+  return (
+    <S.Container>
+     
+    </S.Container>
+  )
+}
+
+export default User;
